e1e8381bd2a7b57962d20762257115674562d539
Rajs <flarecore@gmail.com>
2014-11-21 17:50:35 +0530

    WIP for setting up the overall framework for the application.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   root "feeds#index"
+  get "/collage", to: "feeds#collage"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
